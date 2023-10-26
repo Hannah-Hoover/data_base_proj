abdb6df926704139983896e6c22129ff51845167
mshieldswykes <mshieldswykes@gmail.com>
2023-10-26 18:45:08 -0400

    update init
    updated the init function

--- database/src/userDAO.java
+++ database/src/userDAO.java
@@ -53,7 +53,7 @@ public class userDAO
     public boolean database_login(String email, String password) throws SQLException{
     	try {
     		connect_func("root","pass1234");
-    		String sql = "select * from Client where email = ?";
+    		String sql = "select * from User where email = ?";
     		preparedStatement = connect.prepareStatement(sql);
     		preparedStatement.setString(1, email);
     		ResultSet rs = preparedStatement.executeQuery();
@@ -302,9 +302,9 @@ public class userDAO
     		    	    		"('angelo@gmail.com', 'Angelo', 'Francis', 'angelo1234', '2021-06-14', '4680', 'egypt street', 'lolas', 'DT', '13579', 1000, 0), " +
     		    	    		"('rudy@gmail.com', 'Rudy', 'Smith', 'rudy1234', '1706-06-05', '1234', 'sign street', 'samo ne tu', 'MH', '09876', 1000, 0), " +
     		    	    		"('jeannette@gmail.com', 'Jeannette', 'Stone', 'jeannette1234', '2001-04-24', '0981', 'snoop street', 'kojik', 'HW', '87654', 1000, 0), " +
-    		    	    		"('root', 'default', 'default', 'pass1234', '0000-00-00', '0000', 'Default', 'Default', '0', '00000', 1000, 1000000000);"
+    		    	    		"('root', 'default', 'default', 'pass1234', '0001-01-01', '0000', 'Default', 'Default', '0', '00000', 1000, 1000000000);"
     		    			};
-    		    
+    /*		    
     		   
     		    //client 
     	String[] clientINITIAL = {
@@ -489,7 +489,7 @@ public class userDAO
                             	};
 
 
-                        
+         */               
        					
         
         //for loop to put these in database
@@ -497,7 +497,7 @@ public class userDAO
         	statement.execute(userINITIAL[i]);
         for (int i = 0; i < userTUPLES.length; i++)	
         	statement.execute(userTUPLES[i]);
-        
+        /*
         for (int i = 0; i < clientINITIAL.length; i++)
         	statement.execute(clientINITIAL[i]);
         for (int i = 0; i < clientTUPLES.length; i++)	
@@ -527,7 +527,7 @@ public class userDAO
         	statement.execute(quoteINITIAL[i]);
         for (int i = 0; i < quoteTUPLES.length; i++)	
         	statement.execute(quoteTUPLES[i]);
-        
+        */
         disconnect();
 }
 }
