be71072c7b07cfdb2af2db66b843c85e4863b5a4
Hannah-Hoover <132110786+Hannah-Hoover@users.noreply.github.com>
2023-11-01 14:06:56 -0400

    Create clients.java
    client class file

---
+++ database/src/clients.java
@@ -0,0 +1,91 @@
+public class clients {
+	protected String password;
+ 	protected String email;
+  protected String firstName;
+  protected String lastName;
+  protected String phone;
+	protected String address;
+	protected String creditcard;
+	protected int clientID;
+	
+ 
+    //constructors
+    public clients() {
+    }
+ 
+    public clients(int clientID) 
+    {
+        this.clientID = clientID;
+    }
+    
+    public clients(int clientID, String email,String firstName, String lastName, String password, String address, String phone, String creditcard) 
+    {
+    	this(email, firstName, lastName, password, address, phone, creditcard);
+    	this.clientID = clientID; 
+    }
+ 
+
+    public clients(String firstName, String lastName, String password, String address, String phone, String creditcard, String creditcard2) 
+    {
+    	this.firstName = firstName;
+    	this.lastName = lastName;
+    	this.password = password;
+        this.address = address;
+        this.phone = phone;
+        this.creditcard = creditcard;
+    }
+    
+   //getter and setter methods
+    public int getID() {
+        return clientID;
+    }
+    public void setID(int clientID) {
+        this.clientID = clientID;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getAddress() {
+    	return address;
+    }
+    public void setAddress(String address) {
+    	this.address = address;
+    }
+    public String getPhone() {
+    	return phone;
+    }
+    public void setPhone(String phone) {
+    	this.phone = phone;
+    }
+    public String getCreditcard() {
+    	return creditcard;
+    }
+    public void setCreditcard(String creditcard) {
+    	this.creditcard = creditcard;
+    }
+
+}
