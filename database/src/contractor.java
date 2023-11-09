80a35eb73a21dcb418bec7c1c58713d8a8cea49e
Hannah-Hoover <132110786+Hannah-Hoover@users.noreply.github.com>
2023-11-09 07:54:08 -0500

    Update contractor.java

--- database/src/contractor.java
+++ database/src/contractor.java
@@ -1,61 +1,74 @@
-
-public class contractor 
+public class contractor
 {
-	protected String password;
- 	protected String email;
-    protected String firstName;
-    protected String lastName;
-   
- 
-    //constructors
-    public contractor() {
-    }
- 
-    public contractor(String email) 
-    {
-        this.email = email;
-    }
-    
-    public contractor(String email,String firstName, String lastName, String password) 
-    {
-    	this(firstName,lastName,password);
-    	this.email = email;
-    }
- 
-    public contractor(String firstName, String lastName, String password) 
-    {
-    	this.firstName = firstName;
-    	this.lastName = lastName;
-    	this.password = password;
-    }
-    
-   //getter and setter methods
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-  
-}
+		protected String password;
+	 	protected String email;
+	    protected String firstName;
+	    protected String lastName;
+	    protected int clientID;
+	    
+	   /* public enum UserRole{
+	    	Client, Contractor, Admin
+	    }
+	    
+	    private UserRole role;
+	    */
+	    //constructors
+	    public contractor() {
+	    }
+	 
+	    public contractor(int clientID) 
+	    {
+	        this.clientID = clientID;
+	    }
+	    
+	    public contractor(int clientID, String email,String firstName, String lastName, String password) 
+	    {
+	    	this(email, firstName,lastName,password);
+	    	this.clientID = clientID;
+	    }
+	 
+	
+	    public contractor(String email, String firstName, String lastName, String password) 
+	    {
+	    	this.email = email;
+	    	this.firstName = firstName;
+	    	this.lastName = lastName;
+	    	this.password = password;
+	    }
+	    
+	   //getter and setter methods
+	    public int getID() {
+	        return clientID;
+	    }
+	    public void setID(int clientID) {
+	        this.clientID = clientID;
+	    }
+	    public String getEmail() {
+	        return email;
+	    }
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+	    
+	    public String getFirstName() {
+	        return firstName;
+	    }
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+	    
+	    public String getLastName() {
+	        return lastName;
+	    }
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
+	    
+	    public String getPassword() {
+	        return password;
+	    }
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+
+	}
