905cde5b0dcd8f7e4099cf896129949579590787
Hannah-Hoover <132110786+Hannah-Hoover@users.noreply.github.com>
2023-11-29 13:57:55 -0500

    Update tree.java

--- database/src/tree.java
+++ database/src/tree.java
@@ -1,51 +1,98 @@
 public class tree{
-	protected int clientID;
+	protected int treeID;
+	protected int quoteID;
 	protected String location;
 	protected String height;
 	protected String proximity;
 	protected String diameter;
+	protected String photo1;
+	protected String photo2;
+	protected String photo3;
+	
 	
     //constructors
     public tree() {
     }
-}
- 
-/*    public tree(int clientID) 
+    public tree(int treeID) 
     {
-        this.clientID = clientID;
+        this.treeID = treeID;
     }
     
-    public quote(int clientID, double price, String timeFrame){
-    	this(price, timeFrame);
-    	this.clientID = clientID; 
+    public tree(int treeID, int quoteID, String location, String height, String proximity, String diameter, String photo1, String photo2, String photo3){
+    	this(quoteID, location, height, proximity, diameter, photo1, photo2, photo3);
+    	this.treeID = treeID; 
     }
- 
 
-    public quote(double price, String timeFrame)
+    public quote(quoteID, location, height, proximity, diameter, photo1, photo2, photo3)
     {
-    	this.price = price;
-    	this.timeFrame = timeFrame;
+    	this.quoteID = quoteID;
+    	this.location = location;
+     	this.height = height;
+      	this.proximity = proximity;
+        this.diameter = diameter;
+	this.photo1 = photo1;
+ 	this.photo2 = photo2;
+  	this.photo3 = photo3;
     }
     
    //getter and setter methods
-    public int getID() {
-        return clientID;
+    public int getTreeID() {
+        return treeID;
     }
-    public void setID(int clientID) {
-        this.clientID = clientID;
+    public void setTreeID(int treeID) {
+        this.treeID = treeID;
     }
-    public double getPrice() {
-        return price;
+
+    public int getQuoteID() {
+        return quoteID;
     }
-    public void setPrice(double price) {
-        this.price = price;
+    public void setQuoteID(int quoteID) {
+        this.quoteID = quoteID;
     }
     
-    public String getTimeFrame() {
-        return timeFrame;
+    public String getLocation() {
+        return location;
     }
-    public void setTimeFrame(String timeFrame) {
-        this.timeFrame = timeFrame;
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getProximity() {
+        return proximity;
+    }
+    public void setProximity(String proximity) {
+        this.proximity = proximity;
+    }
+    public String getDiameter() {
+        return diameter;
+    }
+    public void setDiameter(String diameter) {
+        this.diameter = diameter;
+    }
+    public String getPhoto1() {
+        return photo1;
+    }
+    public void setPhoto1(String photo1) {
+        this.photo1 = photo1;
+    }
+    public String getPhoto2() {
+        return photo2;
+    }
+    public void setPhoto2(String photo2) {
+        this.photo2 = photo2;
+    }
+    public String getPhoto3() {
+        return photo3;
+    }
+    public void setPhoto3(String photo3) {
+        this.photo3 = photo3;
     }
 }
-    */
+    
