9d7064c2a82bf10500f1aadcb2ecd228dc2e136e
Hannah-Hoover <132110786+Hannah-Hoover@users.noreply.github.com>
2023-11-29 14:12:16 -0500

    Update and rename quotes.java to quote.java

---
+++ database/src/quote.java
@@ -0,0 +1,69 @@
+public class quote{
+	protected int quoteID;
+	protected int contractorID;
+	protected int clientID
+	protected double price;
+	protected String startTime;
+	protected String endTime;
+	protected String status;	
+ 
+    //constructors
+    public quote() {
+    }    
+
+    public quote(int quoteID){
+	    this.quoteId = quoteID;
+    }
+    public quote(int quoteID, int contractorID, int clientID, double price, String startTime, String endTime, String status){
+	this(contractorID, clientID, price, startTime, endTime, status);
+    	this.quoteID = quoteID; 
+    }
+
+    public quote(int contractorID, int clientID, double price, String startTime, String endTime, String status)
+    {
+	this.contractorID
+    	this.clientID = clientID;
+    	this.startTime = startTime;
+    	this.endTime = endTime;
+    	this.status= status;
+    }
+    
+   //getter and setter methods
+    public int getQuoteID() {
+        return quoteID;
+    }
+    public void setQuoteID(int quoteID) {
+        this.quoteID = quoteID;
+    }
+    public int getClientID() {
+        return clientID;
+    }
+    public void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+    public String getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
