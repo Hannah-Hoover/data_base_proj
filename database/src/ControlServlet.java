d7a105112096928519c8311128376fb5345f770d
Hannah-Hoover <hd3611@wayne.edu>
2023-12-10 14:33:26 -0500

    Update ControlServlet.java

--- database/src/ControlServlet.java
+++ database/src/ControlServlet.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-System.out.println("Here!");
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
